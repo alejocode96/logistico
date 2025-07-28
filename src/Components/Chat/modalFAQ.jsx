a6beb397e8230f5f14a89d41ae6ec1e7f36d5082
alejocode96 <alejocode96@gmail.com>
2025-07-28 10:43:27 -0500

    modal 30

--- src/Components/Chat/modalFAQ.jsx
+++ src/Components/Chat/modalFAQ.jsx
@@ -227,7 +227,7 @@ const ModalFaq = () => {
                                     <label htmlFor="question-select" className="block text-sm font-medium text-gray-700 mb-2 dark:text-gray-300">
                                         Selecciona una pregunta:
                                     </label>
-                                    <select id="question-select" value={selectedFAQ} onChange={handleQuestionChange} className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none bg-gray-50 dark:bg-zinc-800 dark:border-zinc-700 text-zinc-600 dark:text-white " >
+                                    <select id="question-select" value={selectedFAQ} onChange={handleQuestionChange} className="w-full h-12 p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none bg-gray-50 dark:bg-zinc-800 dark:border-zinc-700 text-zinc-600 dark:text-white " >
                                         <option value="">-- Selecciona una opción --</option>
                                         {questions.map((question) => (
                                             <option key={question.id} value={question.id}>
@@ -235,6 +235,8 @@ const ModalFaq = () => {
                                             </option>
                                         ))}
                                     </select>
+
+
                                 </div>
 
                                 {selectedFAQ && (
@@ -603,20 +605,12 @@ const ModalFaq = () => {
                                             <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
                                                 Fecha
                                             </label>
-                                            <div className="relative">
+                                            <div className="grid grid-cols-1 w-full">
                                                 <input
                                                     type="date"
                                                     value={fechaUnica}
                                                     onChange={(e) => setFechaUnica(e.target.value)}
-                                                    className='w-full h-12 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none bg-gray-50 dark:bg-zinc-800 dark:border-zinc-700 text-zinc-600 dark:text-white text-sm min-w-0 max-w-full'
-                                                    style={{ minWidth: 0, maxWidth: '100%' }}
-                                                />
-                                                <CalendarDays
-                                                    className="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-zinc-600 dark:text-white cursor-pointer"
-                                                    onClick={() => {
-                                                        const input = document.querySelector('input[type="date"]');
-                                                        input?.showPicker?.();
-                                                    }}
+                                                    className='col-span-1 w-0 min-w-full h-12 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none bg-gray-50 dark:bg-zinc-800 dark:border-zinc-700 text-zinc-600 dark:text-white text-sm'
                                                 />
                                             </div>
                                         </div>
@@ -627,42 +621,44 @@ const ModalFaq = () => {
                                             <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
                                                 Fecha Inicio
                                             </label>
-                                            <div className="relative">
-                                                <input
-                                                    type="datetime-local"
-                                                    value={fechaInicio}
-                                                    onChange={(e) => setFechaInicio(e.target.value)}
-                                                    className="w-full h-12 px-3 pr-10 border border-gray-300 rounded-lg shadow-sm focus:outline-none bg-gray-50 dark:bg-zinc-800 dark:border-zinc-700 text-zinc-600 dark:text-white text-sm [&::-webkit-calendar-picker-indicator]:opacity-0 min-w-0 max-w-full"
-                                                    style={{ minWidth: 0, maxWidth: '100%' }}
-                                                />
-                                                <CalendarDays
-                                                    className="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-zinc-600 dark:text-white cursor-pointer"
-                                                    onClick={() => {
-                                                        const input = document.querySelector('input[type="datetime-local"]');
-                                                        input?.showPicker?.();
-                                                    }}
-                                                />
+                                            <div className="grid grid-cols-1 w-full">
+                                                <div className="relative col-span-1">
+                                                    <input
+                                                        type="datetime-local"
+                                                        value={fechaInicio}
+                                                        onChange={(e) => setFechaInicio(e.target.value)}
+                                                        className="w-0 min-w-full h-12 px-3 pr-10 border border-gray-300 rounded-lg shadow-sm focus:outline-none bg-gray-50 dark:bg-zinc-800 dark:border-zinc-700 text-zinc-600 dark:text-white text-sm [&::-webkit-calendar-picker-indicator]:opacity-0"
+                                                    />
+                                                    <CalendarDays
+                                                        className="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-zinc-600 dark:text-white cursor-pointer"
+                                                        onClick={() => {
+                                                            const input = document.querySelector('input[type="datetime-local"]');
+                                                            input?.showPicker?.();
+                                                        }}
+                                                    />
+                                                </div>
                                             </div>
                                         </div>
                                         <div>
                                             <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
                                                 Fecha Fin
                                             </label>
-                                            <div className="relative">
-                                                <input
-                                                    type="datetime-local"
-                                                    value={fechaFin}
-                                                    onChange={(e) => setFechaFin(e.target.value)}
-                                                    className="w-full h-12 px-3 pr-10 border border-gray-300 rounded-lg shadow-sm focus:outline-none bg-gray-50 dark:bg-zinc-800 dark:border-zinc-700 text-zinc-600 dark:text-white text-sm [&::-webkit-calendar-picker-indicator]:opacity-0 min-w-0 max-w-full"
-                                                    style={{ minWidth: 0, maxWidth: '100%' }}
-                                                />
-                                                <CalendarDays
-                                                    className="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-zinc-600 dark:text-white cursor-pointer"
-                                                    onClick={() => {
-                                                        const inputs = document.querySelectorAll('input[type="datetime-local"]');
-                                                        inputs[1]?.showPicker?.();
-                                                    }}
-                                                />
+                                            <div className="grid grid-cols-1 w-full">
+                                                <div className="relative col-span-1">
+                                                    <input
+                                                        type="datetime-local"
+                                                        value={fechaFin}
+                                                        onChange={(e) => setFechaFin(e.target.value)}
+                                                        className="w-0 min-w-full h-12 px-3 pr-10 border border-gray-300 rounded-lg shadow-sm focus:outline-none bg-gray-50 dark:bg-zinc-800 dark:border-zinc-700 text-zinc-600 dark:text-white text-sm [&::-webkit-calendar-picker-indicator]:opacity-0"
+                                                    />
+                                                    <CalendarDays
+                                                        className="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-zinc-600 dark:text-white cursor-pointer"
+                                                        onClick={() => {
+                                                            const inputs = document.querySelectorAll('input[type="datetime-local"]');
+                                                            inputs[1]?.showPicker?.();
+                                                        }}
+                                                    />
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
